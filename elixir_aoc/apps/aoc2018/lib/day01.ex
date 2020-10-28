6b463f5c87a91809ed8b0a7f853ef3a51bfff434
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-10-27 20:40:44 -0600

    Update with better solution from internet

--- elixir_aoc/apps/aoc2018/lib/day01.ex
+++ elixir_aoc/apps/aoc2018/lib/day01.ex
@@ -1,36 +1,28 @@
-defmodule Day1 do
-  def part1(file) do
-    case File.read(file) do
-      {:ok, body} -> body
-                      |> parse
-                      |> Enum.sum
-      {:error, reason} -> IO.puts(reason)
-    end
+defmodule Aoc2018.Day01 do
+  use Aoc2018
+
+  def part1(input) do
+    input
+      |> parse
+      |> Enum.sum()
   end
 
-  def part2(file) do
-    case File.read(file) do
-      {:ok, body} -> body
-                      |> parse
-                      |> Stream.cycle()
-                      |> r
-      {:error, reason} -> IO.puts(reason)
-    end
+  def part2(input) do
+    input
+    |> parse
+    |> Stream.cycle()
+    |> r
   end
 
   defp parse(input) do
     input
-    |> String.split("\n", trim: true)
+    |> String.split(~r/\n|, /)
     |> Enum.map(&String.to_integer/1)
   end
 
   defp r(stream) do
     Enum.reduce_while(stream, {0, MapSet.new()}, fn stream, {last, visited} ->
-      # IO.puts(last)
-      # IO.inspect(visited)
       resulting_frequency = last + stream
-
-      # IO.inspect(resulting_frequency)
       if MapSet.member?(visited, resulting_frequency) do
         {:halt, resulting_frequency}
       else
@@ -39,6 +31,3 @@ defmodule Day1 do
     end)
   end
 end
-
-IO.puts(Day1.part1("day1.txt"))
-IO.inspect(Day1.part2("day1.txt"))
