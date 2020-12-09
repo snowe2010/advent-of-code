e26863136b2c8760110112f42d7d8f20e4f8dbc3
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-12-09 00:30:15 -0700

    Y2020 D2P2 Elixir

--- elixir_aoc/apps/aoc2020/lib/day02.ex
+++ elixir_aoc/apps/aoc2020/lib/day02.ex
@@ -8,23 +8,17 @@ aoc 2020, 2 do
 
   def part1s(input) do
     input
-    |> count_incorrect_passwords
+    |> count_incorrect_passwords_with_old_policy
   end
 
-  defp count_incorrect_passwords(parts) do
+  defp count_incorrect_passwords_with_old_policy(parts) do
     parts
-    |> Enum.count(&is_pw_correct?/1)
+    |> Enum.count(&is_pw_correct_with_old_policy?/1)
   end
 
-  defp is_pw_correct?(pw) do
-    # [requirements_full, pw] = pw |> String.split(": ")
-    # [requirements, letter] = requirements_full |> String.split()
-    # [min, max] = String.split(requirements, "-")
-    # IO.inspect(pw <> letter <> min <> max)
-    # IO.puts "Password is " <> pw
-    captures = Regex.named_captures(~r/^(?<min>\d+)-(?<max>\d+) (?<letter>[[:alpha:]]): (?<pw>[[:alpha:]]+)$/, pw)
+  defp is_pw_correct_with_old_policy?(pw) do
+    captures = parse_password_and_requirements(pw)
 
-    # IO.inspect captures
     captures["pw"]
     |> String.codepoints()
     |> Enum.count(fn current_letter -> current_letter == captures["letter"] end)
@@ -35,25 +29,38 @@ aoc 2020, 2 do
     Enum.member?(String.to_integer(min)..String.to_integer(max), num)
   end
 
+
   def part2() do
     stream()
     |> part2s
   end
 
   def part2s(input) do
-    input
+    input |> count_incorrect_passwords_with_new_policy
   end
 
-  defp parse(input) do
-    input
-    |> String.split()
-    |> Enum.map(&String.to_integer/1)
+  defp count_incorrect_passwords_with_new_policy(parts) do
+    parts |> Enum.count(&is_pw_correct_with_new_policy?/1)
+  end
+
+  defp is_pw_correct_with_new_policy?(pw) do
+    captures = parse_password_and_requirements(pw)
+
+    codepoints = captures["pw"]
+                 |> String.codepoints()
+
+    first_matches = Enum.at(codepoints, String.to_integer(captures["min"])-1) == captures["letter"]
+    last_matches = Enum.at(codepoints, String.to_integer(captures["max"])-1) == captures["letter"]
+
+    first_matches != last_matches
+  end
+
+  defp parse_password_and_requirements(pw) do
+    Regex.named_captures(~r/^(?<min>\d+)-(?<max>\d+) (?<letter>[[:alpha:]]): (?<pw>[[:alpha:]]+)$/, pw)
   end
 
   defp stream(), do: input_stream()
-  # |> IO.inspect()
     # |> Stream.map(&String.to_integer/1)
-    # |> IO.inspect()
     # |> Enum.sort()
-    # |> IO.inspect()
+
 end
--- elixir_aoc/apps/aoc2020/test/day02_test.exs
+++ elixir_aoc/apps/aoc2020/test/day02_test.exs
@@ -18,15 +18,36 @@ aoc_test 2020, 2 do
     end
   end
 
-  # describe "part2/1" do
-  #   test "test input results in 241861950" do
-  #     input = """
-  #     1-3 a: abcde
-  #     1-3 b: cdefg
-  #     2-9 c: ccccccccc
-  #     """
 
-  #     assert {2020, 241_861_950} = input |> part2s()
-  #   end
-  # end
+  describe "part2/1" do
+    test "test input has 1 valid passwords" do
+      input = [
+      "1-3 a: abcde",
+      "1-3 b: cdefg",
+      "2-9 c: ccccccccc"
+      ]
+
+      assert 1 = input |> part2s()
+    end
+
+    test "test input has ? valid passwords" do
+      input = [
+        "3-4 j: tjjj", # x
+        "7-10 h: nhhhhhgghphhh", # x
+        "7-13 j: tpscbbstbdjsjbtcpj", # v
+        "4-13 l: ckllmqzlvcsxpplqg", # v
+        "3-11 n: nnrhnnnnnnnwsdnnnm", # v
+        "5-6 d: ddddddb", # x
+        "7-10 z: szfwzrbzzz", # v
+        "1-10 w: wwwwwcwwwrpnwzwxww", # v
+      ]
+
+      assert 5 = input |> part2s()
+    end
+
+
+    test "full input has ? valid passwords" do
+      assert 483 = part2()
+    end
+  end
 end
