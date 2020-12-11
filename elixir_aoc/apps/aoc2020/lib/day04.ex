3b380f46bc1facc1d8c517bf9ea44277d99403fb
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-12-10 22:49:45 -0700

    Y2020-D4-P2 Elixir trying to find bug

--- elixir_aoc/apps/aoc2020/lib/day04.ex
+++ elixir_aoc/apps/aoc2020/lib/day04.ex
@@ -19,42 +19,39 @@ aoc 2020, 4 do
     |> Enum.map(fn x -> Enum.join(x, " ") |> String.split(" ") end)
     |> Enum.map(&map_passport_to_fields/1)
     |> Enum.map(&map_fields_to_requirements/1)
-#    |> IO.inspect
     |> Enum.count(fn mapping ->
       mapping
-      |> Enum.all?(fn {value, requirement} ->
-        IO.puts "checking " <> value
-        matched = String.match?(value, requirement)
-        unless (matched) do
-          IO.puts "failed on "
-          IO.inspect value
-          IO.inspect requirement
-        end
-        matched
-      end)
+      |> Enum.all?(&does_field_match_requirements/1)
     end)
   end
   
   defp map_passport_to_fields(passport) do
-    Enum.into(passport, %{}, fn full_field ->
+    Enum.into(passport, [], fn full_field ->
       split = String.split(full_field, ":")
       {String.to_atom(Enum.at(split, 0)), Enum.at(split, 1)}
     end)
   end
 
   defp map_fields_to_requirements(passport) do
-    requirements = %{
-      byr: ~r/19[2-9][0-9]|200[0-2]/,
-      iyr: ~r/201[0-9]|2020/,
-      eyr: ~r/202[0-9]|2030/,
-      hgt: ~r/1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in/,
-      hcl: ~r/#[0-9a-f]{6}/,
-      ecl: ~r/amb|blu|brn|gry|grn|hzl|oth/,
-      pid: ~r/[0-9]{9}/
-    }
-    
-    for {f1, value} <- passport, {f2, requirements} <- requirements, f1 == f2, into: %{} do
-      {value, requirements}
-    end
+    requirements = [
+      byr: ~r/^19[2-9][0-9]|200[0-2]$/,
+      iyr: ~r/^201[0-9]|2020$/,
+      eyr: ~r/^202[0-9]|2030$/,
+      hgt: ~r/^1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in$/,
+      hcl: ~r/^#[0-9a-f]{6}$/,
+      ecl: ~r/^(amb|blu|brn|gry|grn|hzl|oth)$/,
+      pid: ~r/^[0-9]{9}$/
+    ]
+
+    Keyword.merge(passport, requirements, fn k, v1, v2 -> {v1, v2} end)
   end
+
+  defp does_field_match_requirements({key, requirement_tuple}) when is_tuple(requirement_tuple) do
+    {value, requirement} = requirement_tuple
+    String.match?(value, requirement)
+  end
+
+  defp does_field_match_requirements({key, requirement}) when key == :cid, do: true
+  defp does_field_match_requirements({key, requirement}), do: false
+
 end
