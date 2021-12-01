ade2356549501934a5e745e31c65edc0e5a77b94
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-01 16:57:45 -0700

    I forgot about windowed

--- kotlin_aoc/src/main/kotlin/day01.kt
+++ kotlin_aoc/src/main/kotlin/day01.kt
@@ -4,22 +4,13 @@ fun main() {
 
 fun day01() {
     execute("01", "1") { lines ->
-        val ints = lines.map { it.toInt() }
-        val mutableListOf = mutableListOf(0).apply { addAll(ints) }
-        val message = mutableListOf.zip(ints).drop(1)
-        message.count { (a, b) -> b > a }
+        lines.map { it.toInt() }.windowed(2).count { (a, b) -> b > a }
     }
     execute("01", "2") { lines ->
-        val ints = lines.map { it.toInt() }
-        val list1 = mutableListOf(0).apply { addAll(ints) }
-        val list2 = mutableListOf(0, 0).apply { addAll(ints) }
-        val message = list2.zip(list1)
-            .zip(ints) { a, b -> Triple(a.first, a.second, b) }
-            .drop(2)
+        lines.map { it.toInt() }
+            .windowed(3)
             .map { (a, b, c) -> a + b + c }
-        val list3 = mutableListOf(0).apply { addAll(message) }
-        list3.zip(message)
-            .drop(1)
+            .windowed(2)
             .count { (a, b) -> b > a }
     }
 }
