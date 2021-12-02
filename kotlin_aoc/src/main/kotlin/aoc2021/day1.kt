a7ce145ab8aead4d6d968edaa5beca6bdd6cc052
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-02 14:24:00 -0700

    Clean up kotlin day01 design style
    make it easier to use these functions later if needed

--- kotlin_aoc/src/main/kotlin/aoc2021/day1.kt
+++ kotlin_aoc/src/main/kotlin/aoc2021/day1.kt
@@ -4,15 +4,14 @@ fun main() {
 
 fun day01() {
     day(2021, 1) {
-        execute(1) { lines ->
-            lines.map { it.toInt() }.windowed(2).count { (a, b) -> b > a }
-        }
-
-        execute(2) { lines ->
-            // turns out that b + c + d  > a + b + c is the same as d > a since b and c cancel out
-            lines.map { it.toInt() }
-                .windowed(4)
-                .count { (a, _, _, d) -> d > a }
-        }
+        execute(1) { day01p1(it) }
+        execute(2) { day01p2(it) }
     }
 }
+
+fun day01p1(lines: List<String>) = lines.map { it.toInt() }.windowed(2).count { (a, b) -> b > a }
+
+// turns out that b + c + d  > a + b + c is the same as d > a since b and c cancel out
+fun day01p2(lines: List<String>) = lines.map { it.toInt() }
+    .windowed(4)
+    .count { (a, _, _, d) -> d > a }
