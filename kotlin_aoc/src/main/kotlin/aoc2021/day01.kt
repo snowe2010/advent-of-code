1695b6033b74e0ac1ae04532fc8643ebc368d989
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-01 17:19:38 -0700

    Make kotlin project yearly based, clean up

--- kotlin_aoc/src/main/kotlin/aoc2021/day01.kt
+++ kotlin_aoc/src/main/kotlin/aoc2021/day01.kt
@@ -3,10 +3,10 @@ fun main() {
 }
 
 fun day01() {
-    execute("01", "1") { lines ->
+    execute("2021", "01", "1") { lines ->
         lines.map { it.toInt() }.windowed(2).count { (a, b) -> b > a }
     }
-    execute("01", "2") { lines ->
+    execute("2021", "01", "2") { lines ->
         lines.map { it.toInt() }
             .windowed(3)
             .map { it.sum() }
--- kotlin_aoc/src/main/kotlin/helpers.kt
+++ kotlin_aoc/src/main/kotlin/helpers.kt
@@ -1,31 +1,31 @@
 import java.io.File
 import java.nio.file.Paths
 
-fun execute(day: String, part: String, block: (List<String>) -> Any) {
-    println("======")
-    println("Day $day Part $part")
-    println("======")
+fun execute(year: String, day: String, part: String, block: (List<String>) -> Any) {
+    println("=======================")
+    println("Year $year Day $day Part $part")
+    println("=======================")
     println()
     print("Test: ")
-    test(day, block)
+    test(year, day, block)
     print("Real: ")
-    real(day, block)
+    real(year, day, block)
     println()
 }
 
-fun test(day: String, block: (List<String>) -> Any) {
-    val readLines: List<String> = getInput("day$day.example.txt").readLines()
+fun test(year: String, day: String, block: (List<String>) -> Any) {
+    val readLines: List<String> = getInput(year,"day$day.example.txt").readLines()
     val message = block.invoke(readLines)
     println(message)
 }
 
-fun real(day: String, block: (List<String>) -> Any) {
-    val readLines: List<String> = getInput("day$day.real.txt").readLines()
+fun real(year: String, day: String, block: (List<String>) -> Any) {
+    val readLines: List<String> = getInput(year, "day$day.real.txt").readLines()
     val message = block.invoke(readLines)
     println(message)
 }
 
-fun getInput(filename: String): File {
+fun getInput(year: String, filename: String): File {
     val projectFolder = Paths.get("").toAbsolutePath().toString()
-    return File("$projectFolder/src/main/resources/$filename")
+    return File("$projectFolder/src/main/resources/aoc$year/$filename")
 }
