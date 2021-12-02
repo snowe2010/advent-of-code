8771b40cad30e0cf9bf5f4662351d62bd05e6d5c
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-02 14:11:29 -0700

    ruby d2p1/2 golfed solutions

--- ruby_aoc/2021/day02/day02.rb
+++ ruby_aoc/2021/day02/day02.rb
@@ -10,6 +10,13 @@ execute(1) do |lines|
   hsh['forward'] * (hsh['down'] - hsh['up'])
 end
 
+# golfed
+execute(1) do |lines|
+  h=Hash.new(0)
+  lines.map(&:split).each{h[_1[0]]+=_2.to_i}
+  h[?f].*h[?d]-h[?u]
+end
+
 execute(2) do |lines|
   hp = 0
   aim = 0
@@ -25,4 +32,11 @@ execute(2) do |lines|
   end
   puts "hp #{hp} aim #{aim} depth #{depth}"
   hp * depth
+end
+
+# golfed
+execute(2) do |lines|
+  h,a,d=0,0,0
+  lines.map(&:split).each{|k, i|v=i.to_i;if k[?f];h+=v;d+=(a*v)end;a+=v if k[?n];a-=v if k[?u]}
+  p h*d
 end
