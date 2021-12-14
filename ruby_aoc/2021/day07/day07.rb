480074b3be1a54b9845cce81d4ac956ed62c9972
Tyler B Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-13 22:35:03 -0700

    2021 ruby day7p1/2 code golf

--- ruby_aoc/2021/day07/day07.rb
+++ ruby_aoc/2021/day07/day07.rb
@@ -7,20 +7,30 @@ execute(1, test_only = false) do |lines|
   positions = lines[0].split(',').map(&:to_i)
   min, max = positions.minmax
 
-  sums = min.upto(max).map do |location|
+  min.upto(max).map do |location|
     positions.map { |i| (i - location).abs }.sum
   end.min
-
-  pp sums
 end
 
-execute(2, test_only = false) do |lines|
+execute(1, false, "Code Golf") do |lines|
+  p=lines[0].split(',').map(&:to_i)
+  m,n=p.minmax
+  m.upto(n).map{|l|p.map{(_1-l).abs}.sum}.min
+end
+
+execute(2, false) do |lines|
   positions = lines[0].split(',').map(&:to_i)
   min, max = positions.minmax
 
-  sums = min.upto(max).map do |location|
-    positions.map { |i| a=(i - location).abs;(a**2+a)/2 }.sum
+  min.upto(max).map do |location|
+    positions.map do |i|
+      a = (i - location).abs; (a ** 2 + a) / 2
+    end.sum
   end.min
+end
 
-  pp sums
+execute(2, false, "code golf") do |lines|
+  p=lines[0].split(',').map(&:to_i)
+  m,n=p.minmax
+  m.upto(n).map{|l|p.map{a=(_1-l).abs;(a**2+a)/2}.sum}.min
 end
