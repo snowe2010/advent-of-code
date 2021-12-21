aa8174d6776c5a40ab26b6ceda0df7926873b5b1
Tyler B Thrailkill <tyler.b.thrailkill@gmail.com>
2021-12-21 00:12:29 -0700

    Add script for generating new ruby days

---
+++ ruby_aoc/.template/template.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'pp'
+require_relative '../helpers/input_reader'
+
+execute(1) do |lines|
+end
+
+execute(2) do |lines|
+end
---
+++ ruby_aoc/new_day.rb
@@ -0,0 +1,23 @@
+require 'pathname'
+require 'pp'
+require 'fileutils'
+
+year_dir = if ARGV[0].nil?
+             Pathname('.').each_child.find_all { |child| child.directory? && !child.basename.to_s.start_with?('.') }.max_by { |dir| dir.to_s.to_i }
+           else
+             Pathname(ARGV[0])
+           end
+
+day = if ARGV[1].nil?
+            Pathname(year_dir).glob("day*").select(&:directory?).map(&:to_s).map { |dir| dir.delete_prefix("#{year_dir}/day").to_i}.max + 1
+          else
+            ARGV[1].to_i
+          end
+
+day_dir = "day#{day}"
+
+new_day_path = "#{year_dir.basename}/#{day_dir}"
+FileUtils.mkdir_p(new_day_path)
+FileUtils.cp(".template/template.rb", "#{new_day_path}/#{day_dir}.rb")
+FileUtils.cp(".template/template.real.txt", "#{new_day_path}/#{day_dir}.real.txt")
+FileUtils.cp(".template/template.test.txt", "#{new_day_path}/#{day_dir}.test.txt")
