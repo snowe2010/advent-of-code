1ec9fc9f2e5b42eab11798da02f0bb10a0e82c84
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2023-12-01 23:07:01 -0700

    2023 D02 Ruby Code Golf

--- ruby_aoc/2023/day02/day02.rb
+++ ruby_aoc/2023/day02/day02.rb
@@ -61,3 +61,53 @@ execute(2) do |games|
   end.sum
   p games_out
 end
+
+
+execute(1, alternative_text: "Code Golf") do |games|
+  games_out = games.sum(0) do |game|
+    game_id, rounds = game.split(':')
+    rounds = rounds.split(';')
+    maxes = {
+      "red" => 0,
+      "green" => 0,
+      "blue" => 0,
+    }
+    rounds.map(&:strip).map do |round|
+      round.split(", ").each do |color_count|
+        count, color = color_count.split(" ")
+        maxes[color] = if maxes[color] < count.to_i
+                         count.to_i else maxes[color]
+                       end
+      end
+    end
+    if maxes["red"] <= 12 && maxes["green"] <= 13 && maxes["blue"] <= 14
+      game_id.split(' ')[1].to_i
+    else
+      0
+    end
+  end
+  p games_out
+end
+
+execute(2, alternative_text: "Code Golf Readable") do |games|
+  games_out = games.map do |line|
+    line.sub!(/.*:/,'')
+    maxes = Hash.new(0)
+    line.split(?;) do |round|
+      round.split(?,) do |color_count|
+        count, color = color_count.split
+        maxes[color] = maxes[color]<count.to_i ?count.to_i : maxes[color]
+      end
+    end
+    maxes.each_value.reduce(&:*)
+  end.sum
+  p games_out
+end
+
+execute(2, alternative_text: "Code Golf") do |g|
+p g.map{|l|l.sub!(/.*:/,'')
+m=Hash.new(0)
+l.split(?;){|r|r.split(?,){|a|b,c=a.split
+m[c]=m[c]<b.to_i ? b.to_i: m[c]}}
+m.each_value.reduce(&:*)}.sum
+end
